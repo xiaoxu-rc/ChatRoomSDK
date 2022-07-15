70799d19f16f84fee57fd1c5548abdee38064754
xiaoxu <xiaoxu@rongcloud.cn>
2022-07-15 11:33:35 +0800

    1.删除无用文件

--- Example/Pods/Target Support Files/ChatRoomSDK/ChatRoomSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ChatRoomSDK : NSObject
-@end
-@implementation PodsDummy_ChatRoomSDK
-@end
--- Example/Pods/Target Support Files/Pods-ChatRoomSDK_Example/Pods-ChatRoomSDK_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChatRoomSDK_Example : NSObject
-@end
-@implementation PodsDummy_Pods_ChatRoomSDK_Example
-@end
